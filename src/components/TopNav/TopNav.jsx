d2adf8c8f371f27ff7f4e4707c3d38c92419d18a
zabiremu <zabirraihan570@gmail.com>
2024-02-09 21:16:25 +0600

    fixed icon issues

--- src/components/TopNav/TopNav.jsx
+++ src/components/TopNav/TopNav.jsx
@@ -8,11 +8,6 @@ const TopNav = () => {
           to="/"
           className="flex items-center space-x-3 rtl:space-x-reverse"
         >
-          <img
-            src="https://flowbite.com/docs/images/logo.svg"
-            className="h-8"
-            alt="Flowbite Logo"
-          />
           <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
             Tachnofix
           </span>
